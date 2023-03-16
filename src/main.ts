2fa65793d5279ae61c0e326a4f0a4def0e4f5789
marcoslopez95 <marcoslopez1895@gmail.com>
2023-03-16 17:18:29 -0400

    configurando traslate

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,11 @@ import '@core/scss/index.scss'
 import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
 
+import messages from '@/lang'
 import axios from 'axios'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 
 loadFonts()
 
@@ -27,4 +29,15 @@ app.use(Vue3Toastify, {
   autoClose: 3000,
   theme: 'colored',
 } as ToastContainerOptions);
+
+
+const i18n = createI18n({
+  locale: localStorage.getItem('lang') ?? 'en', // set locale
+  fallbackLocale: 'en', // set fallback locale
+  messages, // set locale messages
+  // If you need to specify other options, you can set other options
+  // ...,
+  legacy: false,
+})
+app.use(i18n)
 app.mount('#app')
